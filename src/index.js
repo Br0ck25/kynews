eb769b68934cfda31e54143078d151d09908d1db
Br0ck25 <jamesbrock25@hotmail.com>
2026-03-21 22:35:42 -0400

    admin console

--- src/index.js
+++ src/index.js
@@ -15,11 +15,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister();
-serviceWorkerRegistration.register();
+// Service worker caching is disabled here because stale SW/precached assets
+// can result in JS chunk mismatch errors (e.g. admin-page-502bebb7.js). See:
+// "Failed to fetch dynamically imported module".
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
